5fadaabee3be6111b724ca89f4a5a54abd46d106
appucyriac <appu@qburst.com>
2017-11-29 20:11:16 +0530

    owl carousel,Footer,Other Ui changes

--- scripts/image_slider.js
+++ scripts/image_slider.js
@@ -1,17 +1,19 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slider_image");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+$(document).ready(function() {
+	$(".owl-carousel").owlCarousel({
+		nav: true,
+		navText: ["<img src='images/next-arrow.png' id='right-arrow'>", "<img src='images/prev-arrow.png' id='left-arrow'>"],
+		autoWidth: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			1024: {
+				items: 2
+			},
+			1224: {
+				items: 3
+			}
+		}
+	});
+});
